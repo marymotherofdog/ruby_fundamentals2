ef0994e8c2fb91e623eb6becf2a147fafd8b320f
Mary Viets <mary.evelyn.viets@gmail.com>
2015-04-01 17:18:46 -0400

    output second item

--- exercise6.rb
+++ exercise6.rb
@@ -7,9 +7,16 @@ end
 
 grocery_list << "rice"
 
-grocery_list
 
 list_items(grocery_list)
 
 p grocery_list.length
+
+if grocery_list.include?("bananas")
+  puts "You need to pick up bananas"
+else puts "You don't need to pick up bananas"
+end
+
+puts grocery_list[1]
+
 
