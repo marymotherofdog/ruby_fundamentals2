fdd49963adf7da351f929b7bc05f7c5ba0e98e09
Mary Viets <mary.evelyn.viets@gmail.com>
2015-04-01 17:11:44 -0400

    added rice to list and counted items

--- exercise6.rb
+++ exercise6.rb
@@ -1,1 +1,15 @@
 grocery_list = ["garlic", "tomatoes", "tape"]
+
+def list_items(array)
+  array.each {|item| puts "* #{item}"}
+end
+
+
+grocery_list << "rice"
+
+grocery_list
+
+list_items(grocery_list)
+
+p grocery_list.length
+
