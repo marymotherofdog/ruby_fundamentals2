b8d87ce45e5863c5871dbcd1e2b888fc05a15b82
Mary Viets <mary.evelyn.viets@gmail.com>
2015-04-01 21:25:56 -0400

    sorted list

--- exercise6.rb
+++ exercise6.rb
@@ -8,15 +8,19 @@ end
 grocery_list << "rice"
 
 
-list_items(grocery_list)
+#list_items(grocery_list)
 
-p grocery_list.length
+#p grocery_list.length
 
-if grocery_list.include?("bananas")
-  puts "You need to pick up bananas"
-else puts "You don't need to pick up bananas"
-end
+#if grocery_list.include?("bananas")
+#  puts "You need to pick up bananas"
+#else puts "You don't need to pick up bananas"
+#end
 
-puts grocery_list[1]
+#puts grocery_list[1]
+
+sorted_groceries = grocery_list.sort
+
+list_items(sorted_groceries)
 
 
