0031bc760401583d130034e905581f1546fddbab
Mary Viets <mary.evelyn.viets@gmail.com>
2015-04-02 00:06:32 -0400

    Total of students in all cohorts

--- exercise7.rb
+++ exercise7.rb
@@ -18,5 +18,21 @@ display(students)
 
 p students.keys
 
+students.each do |key, value|
+  new_cohort = value + (value * 0.05)
+  p new_cohort
+end
+
+students.delete(:cohort2)
+
+display(students)
+
+total = 0
+
+students.each do |key, value|
+  total += value
+end
+
+puts "There are a total of #{total} students."
 
 
